afc5f88e3313a4c491426cd48e7c4b8f24f329ef
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-11-24 23:09:37 +0800

    created functions for gotPredictions and countFingers

--- index.js
+++ index.js
@@ -32,9 +32,69 @@ async function loadHandposeModel(){
     handposeModel.on('predict', gotPrediction)
 }
 
-function gotPrediction(){
+const ctx = canvasEl.getContext('2d')
+canvasEl.width = videoEl.videoWidth
+canvasEl.height = videoEl.videoHeight
 
+function gotPrediction (predictions) {
+    ctx.clearRect(0, 0, canvasEl.width, canvasEl.height)
+    ctx.drawImage(videoEl, 0, 0, canvasEl.width, canvasEl.height)
+
+    if(predictions.length > 0) {
+        const hand = predictions[0]
+        const landmarks = hand.landmarks
+
+        const handLabel = hand.label
+
+        for(let i = 0; i < landmarks.length; i++) {
+            const [x, y, z] = landmarks[i]
+            ctx.beginPath()
+            ctx.arc(x, y, 5, 0, 2 * Math.PI)
+            ctx.fillStyle = 'black'
+            ctx.fill()
+        }
+
+        const fingerCount = countFingers(landmarks, handLabel)
+
+
+    }
 }
+
+function countFingers(landmarks, handLabel) {
+    let fingerCount = 0
+
+    const fingerPairs = [
+        [6, 8],
+        [10, 12],
+        [14,16],
+        [18, 20]
+    ]
+
+    for(const pair of fingerPairs) {
+        const Knuckle_Y = landmarks[pair[0]][1]
+        const Tip_Y = landmarks[pair[1]][1]
+
+        if(Tip_Y < Knuckle_Y){
+            fingerCount++
+        }
+    }
+
+    const thumbTipX = landmarks[4][0]
+    const thumbBaseX = landmarks[2][0]
+
+    if(handLabel === 'Right'){
+        if(thumbTipX > thumbBaseX){
+            fingerCount++
+        }
+    } else if(handLabel === 'Left'){
+        if (thumbTipX < thumbBaseX){
+            fingerCount++
+        }
+    }
+
+    return fingerCount
+}
+
 
 
 init()
